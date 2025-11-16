154d2d484ed2ffe0b582af25527150ae4a6a2226
23h51a05a1@cmrcet.ac.in <123456789>
2025-11-16 18:45:14 +0530

    Second Commit

--- Sum.java
+++ Sum.java
@@ -3,6 +3,6 @@ public class Sum {
     public static void main(String[] args) {
         int a=10,b=20;
         int c=a+b;
-        System.out.println(a+" "+b+"="+c);
+        System.out.println(a+"+"+b+"="+c);
     }
 }
